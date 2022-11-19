f281e613c274251bf16d90cbaa794e345602493c
kmanoovi <ktmandovi@gmail.com>
2022-11-18 19:26:04 -0500

    Added to server.js file

--- 02-Challenge/Develop/server.js
+++ 02-Challenge/Develop/server.js
@@ -1,6 +1,40 @@
 const express = require('express');
+const { read } = require('fs');
 const path = require('path');
-
 const PORT = process.env.PORT || 3001;
-
+const dbData = require('./db/db.json');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/db', (req, res) => res.json(dbData));
+app.post('/api/db', (req, res) => {
+    console.info(`${req.method} request received to add to db`);
+    let response;
+
+    if (req.body && read.body.product) {
+        response = {
+            status: 'success',
+            data: req.body,
+        };
+        res.json(`Note for ${response.data.product} has been added!`);
+    } else {
+        res.json('Request body must contain a product name');
+    }
+    console.log(req.body);
+});
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);
--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Note-Taker
 
-* This project is not finished but I wanted to submit what I have so far. 
-I plan to come back to this project soon once I have a better undstanding of
-Express.js and can seek tutor help.
+A app that allows the user to take notes and save notes via their local storage.
